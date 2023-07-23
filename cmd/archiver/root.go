d6a0ded74de5398304c694a9397ee98bfe3ecaf7
Ask Bjørn Hansen <ask@develooper.com>
2023-07-22 23:24:55 -0700

    Only use explicit configuration file

--- cmd/archiver/root.go
+++ cmd/archiver/root.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -49,16 +48,6 @@ func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".archiver")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/linkedin/goavro/v2 v2.12.0
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.16.0
 )
